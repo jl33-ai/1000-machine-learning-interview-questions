743381cf71a00d0f859cc9da46c4eb331a847d95
Justin <justinkhlee27@gmail.com>
2023-12-25 19:44:46 +1100

    Quartz sync: Dec 25, 2023, 6:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       typography: {
-        header: "Schibsted Grotesk",
+        header: "JetBrains Mono",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
